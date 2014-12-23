efd89742ed00e7aee260703ea3a03c30d99a1c95
ThunderShiviah <thunder@uoregon.edu>
2014-12-22 16:05:00 -0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.todos = [
+      'Item 1',
+      'Item 2',
+      'Item 3'
+    ];
+  });
