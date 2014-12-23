6161330514b2e401a9cd701fb7895785c9430557
ThunderShiviah <thunder@uoregon.edu>
2014-12-22 17:16:53 -0800

    can now add new entries to to-do list.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,8 +10,13 @@
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.todos = [
-      'Item 1',
-      'Item 2',
-      'Item 3'
+    'Item 1',
+    'Item 2',
+    'Item 3',
+    'Item 4'
     ];
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
   });
